4eaee90d3610a2726e250d95d8f889caedee1d50
FA12-BSE-248 <FA12-BSE-248@LAB7C-15.cns.ciit>
2015-11-09 09:36:46 +0500

    done

--- src/Demo1.java
+++ src/Demo1.java
@@ -3,7 +3,9 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("lets play with github");
+		
+		System.out.println(" hii hub");
 	}
 
 }
